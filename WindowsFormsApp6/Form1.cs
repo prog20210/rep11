39ea4dd76a5fa0465f3ae2bd4ccb38e8612fea74
avsemykin2016 <avsemykin2016@yandex.ru>
2022-10-17 18:27:24 +0300

    fix_first_feature

--- WindowsFormsApp6/Form1.cs
+++ WindowsFormsApp6/Form1.cs
@@ -16,5 +16,11 @@ namespace WindowsFormsApp6
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /////hhhhhhhh
+            ///mmmmmmm
+        }
     }
 }
